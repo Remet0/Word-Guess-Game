e3958686b727075fac64c6ac81c873cba81899d4
Remeto <remetojohn@gmail.com>
2019-06-06 19:01:24 -0700

    added a for loop and changed some variable to visualize what was actually going on in my java script. added comments on all my code to remember later

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,33 @@
-var wordArray = ["wordone", "wordtwo", "wordThree"];
-var randomIndex = Math.floor(Math.random()* wordArray.length);
 
+//sets up an array so you can choose a random word
+var wordArray = ["wordone", "wordtwo", "wordthree"];
+//selects a random number that you will later use to get your random word
+var randomIndex = Math.floor(Math.random()* wordArray.length);
+//uses the random number as an index number for your array i.e. chooses a word in the array
 var randomWord = wordArray[randomIndex];
+//sets a score value, using it right now to track if my guesses are registering
+var score = 0;
+//logs the randomword so i can see what is a right guess and what isn't
+console.log(randomWord);
+//set up a score text that will be updated on screen to visualize if i get a correct answer
+var scoreText = document.getElementById("score");
+//sets up a key event that watches for you to let go off your key
+document.onkeyup = function(event){
+//sets a variable to the key you pressed
+var userGuess = event.key;
+
+//a for loop that loops through the random word to search for the letter you pressed
+for (let i = 0; i < randomWord.length; i++) {
+
+//if your guess is one of the letters in the random word then it adds to score
+    if(userGuess === randomWord[i]){
+        score++;
+    }
+    
+}
+//updates the score text before returning
+scoreText.textContent = score;
+return;
+}
+
 
