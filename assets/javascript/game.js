5d05d0dc3b8afa49e620ab2d7eac074715d3e497
Remeto <remetojohn@gmail.com>
2019-06-07 21:04:21 -0700

    created a function to replace letters inside my current string and added and blank array to be a placeholder before the letters are guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,46 @@
 
 //sets up an array so you can choose a random word
 var wordArray = ["wordone", "wordtwo", "wordthree"];
-//selects a random number that you will later use to get your random word
+var blankArray = ["_______", "_______", "_________"];
+//selects a random number that you will later use to get your random word. //uses the random number as an index number for your array i.e. chooses a word in the array
 var randomIndex = Math.floor(Math.random()* wordArray.length);
-//uses the random number as an index number for your array i.e. chooses a word in the array
 var randomWord = wordArray[randomIndex];
+var randomWordPlaceHolder = blankArray[randomIndex];
 //sets a score value, using it right now to track if my guesses are registering
 var score = 0;
 //logs the randomword so i can see what is a right guess and what isn't
 console.log(randomWord);
 //set up a score text that will be updated on screen to visualize if i get a correct answer
+var wordText = document.getElementById("word");
 var scoreText = document.getElementById("score");
-//sets up a key event that watches for you to let go off your key
+
+wordText.textContent = randomWordPlaceHolder;
+
+//a function used to replace text in a string
+function replaceAt(rword, index, replacement){
+     return rword.substr(0, index) + replacement + rword.substr(index + replacement.length);
+
+}
+
+//sets up a key event that watches for you to let go off your key. sets a variable to the key you pressed. //a for loop that loops through the random word to search for the letter you pressed. If your guess is one of the letters in the random word then it adds to score
 document.onkeyup = function(event){
-//sets a variable to the key you pressed
+
 var userGuess = event.key;
-
-//a for loop that loops through the random word to search for the letter you pressed
 for (let i = 0; i < randomWord.length; i++) {
-
-//if your guess is one of the letters in the random word then it adds to score
     if(userGuess === randomWord[i]){
-        score++;
+        console.log(userGuess);
+        randomWordPlaceHolder = replaceAt(randomWordPlaceHolder, i, userGuess);
+        wordText.textContent = randomWordPlaceHolder;
+        console.log(randomWordPlaceHolder);
+        //score++;
     }
     
 }
 //updates the score text before returning
 scoreText.textContent = score;
+
 return;
 }
+
 
 
